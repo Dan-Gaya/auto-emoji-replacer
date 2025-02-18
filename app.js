7e09875622907722b0e57c8cae2964ab6c1a4557
unknown <kaniabdulaziznura@gmail.com>
2025-02-18 10:59:16 +0100

    Initial commit - Auto-Emoji Replacer integration

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const messageRoutes = require('./src/routes/messageRoutes');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const app = express();
+
+//add morgan 
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+// Load routes
+app.get('/', (req,res)=>{
+    res.json(
+        {
+            "welcome":"welcome to AUTO EMOJI REPLACER "
+        }
+    );
+
+});
+app.use('/api', messageRoutes);
+
+
+
+
+module.exports = app;
