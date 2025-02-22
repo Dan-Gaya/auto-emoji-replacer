1d0e10484aa3e48c738754d02a6ee3fb3c3ce10b
Dan-Gaya <kaniabdulaziznura@gmail.com>
2025-02-22 23:48:02 +0100

    integration json updated

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const messageRoutes = require('./src/routes/messageRoutes');
+const integrateSpecRoute =require('./src/routes/integrationSpecRoute')
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+
 
 const app = express();
 
 //add morgan 
 app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
@@ -23,7 +24,7 @@ app.get('/', (req,res)=>{
 
 });
 app.use('/api', messageRoutes);
-
+app.use('/integration-spec', integrateSpecRoute);
 
 
 
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 // Start server
 server.listen(PORT, () => {
-    console.log(`🚀Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
 
 
